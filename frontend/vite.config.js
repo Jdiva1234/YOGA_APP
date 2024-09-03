d417ab5bc132ac54e829843b95404f222f8c9fc8
Justice <122231162+Jdiva1234@users.noreply.github.com>
2024-09-03 22:49:10 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_BACKEND_URL': JSON.stringify('http://localhost:8001'),
-    'process.env.VITE_BACKEND_URL': JSON.stringify('http://localhost:8001'),
-  },
+  define: {},
 })
